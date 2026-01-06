6d11972603713926135f71a2286836aee1e9608b
Eva Chu <eva@Evas-MacBook-Air.local>
2026-01-06 11:19:49 +0800

    chore: add asset typings and ray error catch declarations

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    es2020: true,
+    node: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+};
