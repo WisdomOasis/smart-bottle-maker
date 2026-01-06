15c99767fbc48c2809363a12944468ac27af2369
Eva Chu <eva@Evas-MacBook-Air.local>
2026-01-06 11:40:01 +0800

    chore: remove unused legacy components and assets

--- src/res/index.ts
+++ src/res/index.ts
@@ -1,92 +1,71 @@
-import iconLock from './iconLock.png';
-
-import gps1 from './signal/gps1.png';
-import gps2 from './signal/gps2.png';
-import gps3 from './signal/gps3.png';
-import gps4 from './signal/gps4.png';
-import gpsOff from './signal/gpsOff.png';
-import gps1Light from './signal/gps1Light.png';
-import gps2Light from './signal/gps2Light.png';
-import gps3Light from './signal/gps3Light.png';
-import gps4Light from './signal/gps4Light.png';
-import gpsOffLight from './signal/gpsOffLight.png';
-import wifi1 from './signal/wifi1.png';
-import wifi2 from './signal/wifi2.png';
-import wifi3 from './signal/wifi3.png';
-import wifi4 from './signal/wifi4.png';
-import wifi5 from './signal/wifi5.png';
-import wifi1Light from './signal/wifi1Light.png';
-import wifi2Light from './signal/wifi2Light.png';
-import wifi3Light from './signal/wifi3Light.png';
-import wifi4Light from './signal/wifi4Light.png';
-import wifi5Light from './signal/wifi5Light.png';
-import bluetooth from './signal/bluetooth.png';
-import bluetoothDisconnect from './signal/bluetoothBlack.png';
-
-import batNull from './battery/batNull.png';
-import batHigh from './battery/batHigh.png';
-import batMiddle2 from './battery/batMiddle2.png';
-import batMiddle from './battery/batMiddle.png';
-import batLow from './battery/batLow.png';
-import batHighLight from './battery/batHighLight.png';
-import batMiddle2Light from './battery/batMiddle2Light.png';
-import batMiddleLight from './battery/batMiddleLight.png';
-import batLowLight from './battery/batLowLight.png';
-
-import defaultCar from './defaultCar.png';
-import modeChange from './modeChange.png';
-import modeChangeLight from './modeChangeLight.png';
-import batWarn from './batWarn.png';
-import batWarnLight from './batWarnLight.png';
-import iconLocation from './iconLocation.png';
-import iconLocationLight from './iconLocationLight.png';
-import iconTracks from './iconTracks.png';
-import iconTracksLight from './iconTracksLight.png';
-import iconSet from './iconSet.png';
-import iconSetLight from './iconSetLight.png';
+import airbuggyLogo from "./airbuggy_logo.svg";
+import petDog from "./pet_dog.svg";
+import petDogActive from "./pet_dog_active.svg";
+import petCat from "./pet_cat.svg";
+import petCatActive from "./pet_cat_active.svg";
+import petSquirrel from "./pet_squirrel.svg";
+import petSquirrelActive from "./pet_squirrel_active.svg";
+import petHamster from "./pet_hamster.svg";
+import petHamsterActive from "./pet_hamster_active.svg";
+import petRabbit from "./pet_rabbit.svg";
+import petRabbitActive from "./pet_rabbit_active.svg";
+import petBird from "./pet_bird.svg";
+import petBirdActive from "./pet_bird_active.svg";
+import petOther from "./pet_other.svg";
+import petOtherActive from "./pet_other_active.svg";
+import petDogWhite from "./pet_dog_white.svg";
+import petCatWhite from "./pet_cat_white.svg";
+import petSquirrelWhite from "./pet_squirrel_white.svg";
+import petHamsterWhite from "./pet_hamster_white.svg";
+import petRabbitWhite from "./pet_rabbit_white.svg";
+import petBirdWhite from "./pet_bird_white.svg";
+import petOtherWhite from "./pet_other_white.svg";
+import startIcon from "./ic_arrow_right.svg";
+import icNotification from "./ic_notification.svg";
+import icSettings from "./ic_settings.svg";
+import icConnected from "./ic_connected.svg";
+import icOffline from "./ic_offline.svg";
+import icPowerOn from "./ic_power_on.svg";
+import icPowerOnActive from "./ic_power_on_active.svg";
+import icPowerOff from "./ic_power_off.svg";
+import icPowerOffActive from "./ic_power_off_active.svg";
+import temperatureBalance from "./temperature_balance.png";
+import temperatureHigh from "./temperature_high.png";
+import temperatureLow from "./temperature_low.png";
 
 export default {
-  iconLock,
-  gps1,
-  gps2,
-  gps3,
-  gps4,
-  gpsOff,
-  gps1Light,
-  gps2Light,
-  gps3Light,
-  gps4Light,
-  gpsOffLight,
-  wifi1,
-  wifi2,
-  wifi3,
-  wifi4,
-  wifi5,
-  wifi1Light,
-  wifi2Light,
-  wifi3Light,
-  wifi4Light,
-  wifi5Light,
-  bluetooth,
-  bluetoothDisconnect,
-  defaultCar,
-  modeChange,
-  modeChangeLight,
-  batNull,
-  batHigh,
-  batMiddle,
-  batMiddle2,
-  batLow,
-  batHighLight,
-  batMiddle2Light,
-  batLowLight,
-  batWarn,
-  batWarnLight,
-  batMiddleLight,
-  iconLocation,
-  iconLocationLight,
-  iconTracks,
-  iconTracksLight,
-  iconSet,
-  iconSetLight,
+  airbuggyLogo,
+  petDog,
+  petDogActive,
+  petCat,
+  petCatActive,
+  petSquirrel,
+  petSquirrelActive,
+  petHamster,
+  petHamsterActive,
+  petRabbit,
+  petRabbitActive,
+  petBird,
+  petBirdActive,
+  petOther,
+  petOtherActive,
+  petDogWhite,
+  petCatWhite,
+  petSquirrelWhite,
+  petHamsterWhite,
+  petRabbitWhite,
+  petBirdWhite,
+  petOtherWhite,
+  startIcon,
+  icNotification,
+  icSettings,
+  icConnected,
+  icOffline,
+  icPowerOn,
+  icPowerOnActive,
+  icPowerOff,
+  icPowerOffActive,
+  temperatureBalance,
+  temperatureHigh,
+  temperatureLow,
 };
